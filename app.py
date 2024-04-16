6ebb8db3f47153bb0cdd29de09518de760a5ea97
Arthur Malveste <arthurmielemalveste@gmail.com>
2024-04-15 21:26:15 -0300

    config rotas erros

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, jsonify, redirect, render_template, request
+from flask import Flask, jsonify, redirect, render_template, request, session
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-
     return render_template('index.html')
+
+
+# Manipulador de erro para página não encontrada (Erro 404)
+@app.errorhandler(404)
+def pagina_nao_encontrada_erro(error):
+    return render_template('404.html'), 404
+
+# Manipulador de erro para erro interno do servidor (Erro 500)
+@app.errorhandler(500)
+def erro_interno_erro(error):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
